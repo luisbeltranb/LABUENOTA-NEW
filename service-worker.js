99dc21bd55dcf80052354293e0b1424dc8ce8863
luisbeltranb <98564888+luisbeltranb@users.noreply.github.com>
2025-06-06 22:08:48 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,15 @@
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('la-buenota-cache').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './icon-192.png',
-        './icon-512.png'
-      ]);
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("radio-cache").then(cache => {
+      return cache.addAll(["./", "./index.html", "./manifest.json"]);
     })
   );
 });
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
     })
   );
 });
