a4cd8092401a4f2e72a7f62a16737b513f47ef8e
luisbeltranb <98564888+luisbeltranb@users.noreply.github.com>
2025-06-06 22:14:23 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,23 @@
-self.addEventListener("install", function(e) {
+
+self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open("labuenota").then(function(cache) {
-      return cache.addAll(["index.html"]);
+    caches.open('buenota-radio-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/service-worker.js',
+        '/assets/background.png',
+        '/icons/icon.png'
+      ]);
     })
   );
 });
 
-self.addEventListener("fetch", function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
 });
